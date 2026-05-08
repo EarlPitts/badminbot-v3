29f60b28e29ce7503e524880c2e334c0c76b250c
Bendeguz Seres <grafitpor@gmail.com>
2026-05-08 08:03:05 +0200

    Add crucial feature

--- src/Bot.hs
+++ src/Bot.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Bot (runBot) where
 
 import Control.Monad (unless, void, when)
 import Control.Monad.IO.Class
-import Data.Text (Text, isPrefixOf, pack, toLower)
+import Data.Text (Text, isPrefixOf, lines, pack, toLower)
 import qualified Data.Text.IO as TIO
 import System.Environment
+import System.Random (randomRIO)
 import UnliftIO.Concurrent
+import Prelude hiding (lines)
 
 import Discord
 import qualified Discord.Requests as R
@@ -15,6 +18,18 @@ import Discord.Types
 
 import Command
 import qualified Poll as P
+
+data Env = Env
+  { -- config :: Config
+    jokes :: [Text]
+  }
+  deriving (Show)
+
+-- data Config = Config
+--   { days :: [Int]
+--   , slots :: [Int]
+--   }
+--   deriving (Show)
 
 runBot :: IO ()
 runBot = do
@@ -24,11 +39,13 @@ runBot = do
 badminbot :: Text -> IO ()
 badminbot token = do
   putStrLn "started..."
+  js <- lines <$> TIO.readFile "jokes.txt"
+  let env = Env js
   userFacingError <-
     runDiscord $
       def
         { discordToken = "Bot " <> token
-        , discordOnEvent = eventHandler
+        , discordOnEvent = eventHandler env
         , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
         }
   TIO.putStrLn userFacingError
@@ -36,23 +53,35 @@ badminbot token = do
 -- userFacingError is an unrecoverable error
 -- put normal 'cleanup' code in discordOnEnd (see examples)
 
-eventHandler :: Event -> DiscordHandler ()
-eventHandler event = case event of
-  MessageCreate msg -> handleMessage msg
+eventHandler :: Env -> Event -> DiscordHandler ()
+eventHandler env event = case event of
+  MessageCreate msg -> handleMessage env msg
   -- MessageCreate m -> when (isPing m && not (fromBot m)) $ do
   --   void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
   --   threadDelay (2 * 10 ^ 6)
   --   void $ restCall (R.CreateMessage (messageChannelId m) "Pong!")
   _ -> return ()
 
-handleMessage :: Message -> DiscordHandler ()
-handleMessage msg = unless (fromBot msg) $ do
+handleMessage :: Env -> Message -> DiscordHandler ()
+handleMessage Env{..} msg = unless (fromBot msg) $ do
   let cmd = getCmd (messageContent msg)
   case cmd of
-    Right CreatePoll -> do
-      P.PollResponse url <- liftIO P.createPoll -- TODO retry if didn't succeed
-      void $ restCall (R.CreateMessage (messageChannelId msg) url) -- TODO retry this too
-    Left _ -> void $ restCall (R.CreateMessage (messageChannelId msg) "Sorry, didn't understand :(")
+    Right CreatePoll -> createPoll msg
+    Right TellJoke -> tellJoke jokes msg
+    Left _ -> unknown msg
+
+createPoll :: Message -> DiscordHandler ()
+createPoll msg = do
+  P.PollResponse url <- liftIO P.createPoll -- TODO retry if didn't succeed
+  void $ restCall (R.CreateMessage (messageChannelId msg) url) -- TODO retry this too
+
+tellJoke :: [Text] -> Message -> DiscordHandler ()
+tellJoke jokes msg = do
+  joke <- (jokes !!) <$> randomRIO (0, length jokes - 1)
+  void $ restCall (R.CreateMessage (messageChannelId msg) joke) -- TODO retry this too
+
+unknown :: Message -> DiscordHandler ()
+unknown msg = void $ restCall (R.CreateMessage (messageChannelId msg) "Sorry, didn't understand :(")
 
 fromBot :: Message -> Bool
 fromBot = userIsBot . messageAuthor
--- src/Command.hs
+++ src/Command.hs
@@ -3,15 +3,21 @@ module Command where
 import Data.Functor
 import Data.Text
 import Text.Parsec
-import Text.Parsec.String
+import Text.Parsec.Text
 
-data Command = CreatePoll deriving (Show)
+data Command
+  = CreatePoll
+  | TellJoke
+  deriving (Show)
 
 getCmd :: Text -> Either ParseError Command
-getCmd msg = parse pCommand "command" (unpack msg)
+getCmd = parse pCommand "command"
 
 pCommand :: Parser Command
-pCommand = char '!' *> choice [pPoll]
+pCommand = char '!' *> choice [pPoll, pJoke]
 
 pPoll :: Parser Command
 pPoll = string "poll" $> CreatePoll
+
+pJoke :: Parser Command
+pJoke = string "joke" $> TellJoke
